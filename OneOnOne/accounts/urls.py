28d5191361d39236ac3decd343c21801d9071aa3
Jazli14 <95947726+Jazli14@users.noreply.github.com>
2024-03-06 02:06:02 -0500

    fixed url names

--- OneOnOne/accounts/urls.py
+++ OneOnOne/accounts/urls.py
@@ -5,11 +5,9 @@ from .views import test_endpoint, create_user_account, handle_contacts
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('test/', test_endpoint, name='create_user'),
-    path('users/', create_user_account),
-    path('users/<int:user_id>/contacts/', handle_contacts, name='add_contact'),
-    # path('users/<int:user_id>/contacts/<int:contact_id>/', get_contact, name='get_contact'),
-    # path('users/<int:user_id>/contacts/<int:contact_id>/', update_contact, name='update_contact'),
-    # path('users/<int:user_id>/contacts/<int:contact_id>/', delete_contact, name='delete_contact'),
+    path('test/', test_endpoint),
+    path('users/', create_user_account, name='create_user'),
+    path('users/<int:user_id>/contacts/', handle_contacts, name='handle_contacts'),
+    # path('users/<int:user_id>/contacts/<int:contact_id>/', handle_specific_contacts, name='get_contact'),
 
 ]
