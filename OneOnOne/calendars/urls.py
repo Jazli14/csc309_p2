e6aea2ec47f8fe06af8b50cf99cee605e3de1d42
ibrahim681 <81816993+ibrahim681@users.noreply.github.com>
2024-03-11 10:04:05 -0400

    added CRUD for calendar

--- OneOnOne/calendars/urls.py
+++ OneOnOne/calendars/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import test_ping
+from .views import test_ping, CalendarListCreate, CalendarRetrieveUpdateDestroy 
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('test/', test_ping)
+    path('test/', test_ping),
+    path('calendar/', CalendarListCreate.as_view(), name='calendar-list'),
+    path('calendar/<int:pk>/', CalendarRetrieveUpdateDestroy.as_view(), name='calendar-detail'),
 ]
--- OneOnOne/calendars/views.py
+++ OneOnOne/calendars/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from rest_framework import status
+from .models import Calendar
+from rest_framework import status, generics
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
+from .serializers import CalendarSerializer
 
 # Create your views here.
 @api_view(['GET'])
 def test_ping(request):
     return Response({"data": "Server is up and running."})
+
+class CalendarListCreate(generics.ListCreateAPIView):
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
+
+class CalendarRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Calendar.objects.all()
+    serializer_class = CalendarSerializer
