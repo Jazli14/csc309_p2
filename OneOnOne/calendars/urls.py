82a480309cf662b9e0cd756b148829b66651b6f6
Sami <81816993+ibrahim681@users.noreply.github.com>
2024-03-11 16:20:07 -0400

    Update urls.py

--- OneOnOne/calendars/urls.py
+++ OneOnOne/calendars/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import test_ping, CalendarListCreate, CalendarRetrieveUpdateDestroy 
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('test/', test_ping),
     path('calendar/', CalendarListCreate.as_view(), name='calendar-list'),
     path('calendar/<int:pk>/', CalendarRetrieveUpdateDestroy.as_view(), name='calendar-detail'),
