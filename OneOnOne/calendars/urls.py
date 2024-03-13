3ac9a3b55ecd908eb292be964f66e4dd3dd442a7
Kaiwen Tan <tankai3@teach.cs.toronto.edu>
2024-03-13 01:02:31 -0400

    implemented adding students to calendars

--- OneOnOne/calendars/urls.py
+++ OneOnOne/calendars/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import test_ping, CalendarListCreate, CalendarRetrieveUpdateDestroy, TimeBlocksListView, AddTimeBlockView, EditTimeBlockView, DeleteTimeBlockView 
+from .views import CalendarAddStudent, test_ping, CalendarListCreate, CalendarRetrieveUpdateDestroy, TimeBlocksListView, AddTimeBlockView, EditTimeBlockView, DeleteTimeBlockView 
 
 urlpatterns = [
     path('test/', test_ping),
     path('calendar/', CalendarListCreate.as_view(), name='calendar-list'),
     path('calendar/<int:pk>/', CalendarRetrieveUpdateDestroy.as_view(), name='calendar-detail'),
+    path('calendars/<int:calendar_id>/add_student/', CalendarAddStudent.as_view(), name='calendar-add-student'),
     path('calendar/<int:calendar_id>/timeblocks/', TimeBlocksListView.as_view(), name='timeblocks-list'),
     path('calendar/<int:calendar_id>/timeblocks/add/', AddTimeBlockView.as_view(), name='add-timeblock'),
     path('timeblocks/<int:pk>/', EditTimeBlockView.as_view(), name='edit-timeblock'),
--- OneOnOne/calendars/views.py
+++ OneOnOne/calendars/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from accounts.models import UserAccount
 from .models import Calendar, TimeBlock
 from rest_framework import status, generics
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
@@ -10,19 +12,61 @@ from .serializers import CalendarSerializer, TimeBlockSerializer
 
 # Calendar Views 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def test_ping(request):
     return Response({"data": "Server is up and running."})
 
+@permission_classes([IsAuthenticated])
 class CalendarListCreate(generics.ListCreateAPIView):
     queryset = Calendar.objects.all()
     serializer_class = CalendarSerializer
 
+@permission_classes([IsAuthenticated])
 class CalendarRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Calendar.objects.all()
     serializer_class = CalendarSerializer
 
 
+@permission_classes([IsAuthenticated])
+class CalendarAddStudent(generics.GenericAPIView):
+    serializer_class = CalendarSerializer
+    
+    def patch(self, request, *args, **kwargs):
+        calendar_id = kwargs.get('pk')  
+        student_user_id = request.data.get('student_user_id')
+        
+        if not student_user_id:
+            return Response({"error": "need student id"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+        try:
+            calendar = Calendar.objects.get(id=calendar_id)
+            student = UserAccount.objects.get(user__id=student_user_id)
+            if student.user_type == "t":
+                return Response({"error": "can't add a teacher"}, status=status.HTTP_400_BAD_REQUEST)
+            
+        except Calendar.DoesNotExist:
+            return Response({"error": "calendar not found"}, status=status.HTTP_404_NOT_FOUND)
+        except UserAccount.DoesNotExist:
+            return Response({"error": "student not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        calendar.students.add(student)
+        calendar.save()  
+        
+        student_data = {
+            "id": student.id,
+            "username": student.username
+        }
+        
+        return Response({
+            "success": "Student added to calendar",
+            "student": student_data
+        }, status=status.HTTP_200_OK)
+
+
+
 # TimeBlock Views
+@permission_classes([IsAuthenticated])
 class TimeBlocksListView(generics.ListAPIView):
     serializer_class = TimeBlockSerializer
 
@@ -30,7 +74,7 @@ class TimeBlocksListView(generics.ListAPIView):
         calendar_id = self.kwargs['calendar_id']
         return TimeBlock.objects.filter(calendar_id=calendar_id)
 
-
+@permission_classes([IsAuthenticated])
 class AddTimeBlockView(generics.CreateAPIView):
     serializer_class = TimeBlockSerializer
 
@@ -47,6 +91,7 @@ class AddTimeBlockView(generics.CreateAPIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+@permission_classes([IsAuthenticated])
 class EditTimeBlockView(generics.RetrieveUpdateAPIView):
     serializer_class = TimeBlockSerializer
     queryset = TimeBlock.objects.all()
@@ -66,6 +111,7 @@ class EditTimeBlockView(generics.RetrieveUpdateAPIView):
         serializer.save()
         return Response(serializer.data)
 
+@permission_classes([IsAuthenticated])
 class DeleteTimeBlockView(generics.DestroyAPIView):
     serializer_class = TimeBlockSerializer
     queryset = TimeBlock.objects.all()
