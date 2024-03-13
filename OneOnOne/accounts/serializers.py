ce0d8a581163430d90f9e5b7478d6fdc28882977
Jazli14 <95947726+Jazli14@users.noreply.github.com>
2024-03-13 01:51:07 -0400

    Created login, update account, delete contact and account

--- OneOnOne/accounts/serializers.py
+++ OneOnOne/accounts/serializers.py
@@ -21,31 +21,3 @@ class ContactSerializer(serializers.ModelSerializer):
 
     def get_email(self, obj):
         return obj.contact.email if obj.contact else None
-
-    # def validate(self, data):
-    #     # Check if the contact is trying to add themselves
-    #     if self.context['request'].user == data.get('contact'):
-    #         return Response(
-    #             {"error": "You cannot add yourself as a contact."},
-    #             status=status.HTTP_409_CONFLICT
-    #         )
-    #     return data
-
-    # def create(self, validated_data):
-    #     user_id = validated_data.pop('user_id')
-    #     contact = Contact.objects.create(user_id=user_id, **validated_data)
-    #     return contact
-
-
-    # def validate(self, data):
-    #     print("WE IN VALIDATION?")
-
-    #     # Check if the contact already exists
-    #     user_id = data.get('user_id')
-    #     contact_id = data.get('contact')
-    #     if Contact.objects.filter(user=user_id, contact=contact_id).exists():
-    #         print("WE IN HERE?")
-    #         raise serializers.ValidationError("Contact already exists in the list")
-    #     print("WE OUT HEERE?")
-    #     return data
-
